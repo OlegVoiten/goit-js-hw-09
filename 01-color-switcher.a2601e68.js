4e5fd4c7671d0137a9fc7c4df7e5befc14f123e9
OlegVoiten <103588890+OlegVoiten@users.noreply.github.com>
2023-07-06 14:33:40 +0000

    Deploying to gh-pages from @ OlegVoiten/goit-js-hw-09@9f07d4ebdb55c9137c6dbb458c7a915faa09bde7 🚀

---
+++ 01-color-switcher.a2601e68.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let d=null;t.addEventListener("click",()=>{t.disabled=!0,d=setInterval(()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`},1e3)}),e.addEventListener("click",()=>{t.disabled=!1,clearInterval(d)});
+//# sourceMappingURL=01-color-switcher.a2601e68.js.map
